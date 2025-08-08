0f3a11b42ef61dbf87d964c33a2e41820311b91d
Arion Kennedy <ariondonar@hotmail.com>
2025-08-07 21:22:52 -0500

    Changed the sidebar to work properly and prevent any current bugs. Simplified the code and made it more efficient.

---
+++ scripts/standard.js
@@ -0,0 +1,15 @@
+function nav_open() {
+  document.getElementById("mySidebar").classList.add("open");
+  document.getElementById("myOverlay").classList.add("open");
+  document.getElementById("sideBarLineOne").classList.add("wide");
+  document.getElementById("sideBarLineTwo").classList.add("wide");
+  document.getElementById("sideBarLineThree").classList.add("wide");
+}
+
+function nav_close() {
+  document.getElementById("mySidebar").classList.remove("open");
+  document.getElementById("myOverlay").classList.remove("open");
+  document.getElementById("sideBarLineOne").classList.remove("wide");
+  document.getElementById("sideBarLineTwo").classList.remove("wide");
+  document.getElementById("sideBarLineThree").classList.remove("wide");
+}
